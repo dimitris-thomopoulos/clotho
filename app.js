314d6688382cda50357059f7c1d11bd0027acf2b
dimitris-thomopoulos <thomopoulos59@gmail.com>
2021-06-07 03:08:48 +0300

    Improved burger icon animation

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const burgerButton = document.querySelector('.burger');
-const topLine = document.querySelector('.burger-lines span:nth-of-type(1)');
-const bottomLine = document.querySelector('.burger-lines span:nth-of-type(2)');
+
+const topLine = document.querySelector('.topLine');
+const topLineWhite = document.querySelector('.topLine span:nth-of-type(2)');
+const topLineTransp = document.querySelector('.topLine span:nth-of-type(1)');
+
+const bottomLine = document.querySelector('.bottomLine');
+const bottomLineWhite = document.querySelector('.bottomLine span:nth-of-type(2)');
+const bottomLineTransp = document.querySelector('.bottomLine span:nth-of-type(1)');
 const linesContainer = document.querySelector('.burger-lines');
 
 const headerButton = document.querySelector('#header-btn');
@@ -9,17 +15,24 @@ const manualIndices = document.querySelector('.navigation-manual');
 const autoIndices = document.querySelector('.navigation-auto');
 
 burgerButton.addEventListener('click', () => {
+    
+    topLine.style.transform = 'translateX(15px) rotate(45deg)';
+    topLineWhite.style.width = '62px';
+    topLineTransp.style.width = '28px';
 
-    linesContainer.style.height = '100%';
-    linesContainer.style.justifyContent = 'center';
-    linesContainer.style.alignItems = 'center';
-    linesContainer.style.transition = 'all 0.3s ease-in-out';
-    
-    topLine.style.transform = 'rotate(-45deg) translateY(1.5px)';
     topLine.style.transition = 'all 0.3s ease-in-out';
+    topLineWhite.style.transition = 'all 0.3s ease-in-out';
+    topLineTransp.style.transition = 'all 0.3s ease-in-out';
     
-    bottomLine.style.transform = 'rotate(45deg) translateY(-1.5px)';
+
+
+    bottomLine.style.transform = 'translateX(15px) rotate(-45deg)';
+    bottomLineWhite.style.width = '62px';
+    bottomLineTransp.style.width = '28px';
+    
     bottomLine.style.transition = 'all 0.3s ease-in-out';
+    bottomLineWhite.style.transition = 'all 0.3s ease-in-out';
+    bottomLineTransp.style.transition = 'all 0.3s ease-in-out';
 
     headerButton.style.display = 'none';
     clothoTitle.style.display = 'none';
